d0245f42b88f3e0633cbcfcdabedc497f34ec380
Harshit Lohani <harshitlohani2002@gmail.com>
2024-04-07 10:39:24 +0530

    Codewars repo upload

---
+++ Detect Pangram.py
@@ -0,0 +1,9 @@
+def is_pangram(s):
+    l=len(s)
+    s=s.lower()
+    sl=s.list()
+    for i in range (l):
+        for j in range(l):
+            if()
+
+is_pangram("The quick brown fox jumps over the lazy dog")
